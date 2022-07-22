b17f049eff68d124ed1ece8203cc8bb9260ad0a5
Camilo Rodriguez <caarodriguezhe@gmail.com>
2022-07-22 17:26:44 +0000

    New Action

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import { onMount } from "svelte";
+    let data = [];
+
+    onMount(async() => {
+        const res = await fetch("synthesis_actions.json")
+        data = await res.json();
+    });
+</script>
+
+{#each data as action}
+    <div class="action">
+        <h2>{action.name}</h2>
+        <p>{action.description}</p>
+        <img src="{action.icon}" alt="">
+    </div>
+{/each}
