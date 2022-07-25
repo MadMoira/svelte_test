760be7a07f3b87033d50e671077209bcb0ab5935
Camilo Rodriguez <caarodriguezhe@gmail.com>
2022-07-25 03:32:44 +0000

    Some new libraries and start working on the initial macro functionality

---
+++ src/lib/components/ListActions.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { actions } from '$lib/stores/actions_store.js';
+
+    export let data;
+    export let title;
+
+    function handleClick(action) {
+        actions.update(value => [...value, action]) ;
+    }
+</script>
+
+<div class="container md-mx-auto border-2">
+    <p class="mb-6">{title} Actions</p>
+    <div class="grid grid-cols-3">
+        {#each data as action}
+        <div class="grid justify-items-center" on:click={() => handleClick(action.name)}>
+            <img style="width: 32px" src="{action.icon}" alt="">
+            <p>{action.name}</p>
+        </div>
+        {/each}
+    </div>
+</div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,17 +1,49 @@
 <script>
-import { onMount } from "svelte";
-    let data = [];
+    import { onDestroy } from 'svelte';
 
-    onMount(async() => {
-        const res = await fetch("synthesis_actions.json")
-        data = await res.json();
-    });
+    import synt_data from '$lib/synthesis_actions.json';
+    import touch_data from '$lib/touch_actions.json';
+    import buff_data from '$lib/buff_actions.json';
+    import other_data from '$lib/other_actions.json';
+
+    import { actions } from '$lib/stores/actions_store.js';
+    import ListActions from '$lib/components/ListActions.svelte';
+    
+    let currentActions = [];
+    $: macro_text = currentActions.map(function(action){
+        return `\\ac "${action}" <wait.3>`;
+    }).join("<br>");
+     
+    const actions_unsubscribe = actions.subscribe(value => currentActions = value);
+
+    function copyMacroToClipboard() {
+        let result = currentActions.map(function(action) {
+            return `\\ac "${action}" <wait.3>`;
+        }).join("\n");
+        navigator.clipboard.writeText(result);
+    }
+
+    function resetMacro() {
+        actions.update(actions => actions = []);
+    }
+
+    onDestroy(actions_unsubscribe);
 </script>
 
-{#each data as action}
-    <div class="action">
-        <h2>{action.name}</h2>
-        <p>{action.description}</p>
-        <img src="{action.icon}" alt="">
+<div class="flex flex-row">
+    <div class="basis-1/2">
+        <ListActions title="Synthesis" data={synt_data} />
+        <ListActions title="Touch" data={touch_data} />
+        <ListActions title="Buff" data={buff_data} />
+        <ListActions title="Other" data={other_data} />
     </div>
-{/each}
+    <div class="basis-1/2">
+        <button class="btn-primary" on:click={copyMacroToClipboard}>Copy macro</button>
+        <button class="btn-primary" on:click={resetMacro}>Clear macro</button>
+        <p>{@html macro_text}</p>
+    </div>
+</div>
+
+    
+
+
